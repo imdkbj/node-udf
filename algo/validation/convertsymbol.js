5509a967710b4f2abf8e35de7d207e942c79baf5
Deepak Bhardwaj <imdkbj@gmail.com>
2022-12-30 10:00:01 +0530

    UPDATED SYMBOL FORMAT

--- algo/validation/convertsymbol.js
+++ algo/validation/convertsymbol.js
@@ -1,87 +1,100 @@
 const StringUDF = require("../../string_udf/stringUDF");
 
 class ConvertToSymbol extends StringUDF {
-    constructor() {
-        super()
+  constructor() {
+    super();
+  }
+
+  insertYear = (scrip, scripType) => {
+    var _year = 22;
+
+    let year_index = scripType == "FUT" ? -2 : -3;
+    let oldYear = scrip.slice(year_index, year_index + 1)[0];
+    let is_year = this.isNumeric(oldYear);
+
+    if (is_year) return scrip;
+
+    return this.insertInToArray(scrip, year_index + 1, _year);
+  };
+
+  // ************************************************************************************************
+  // convertToTradingSymbol
+  //
+  // Validate the input of the order send on the bot.
+  // It will have one arguments.
+  // 1. Pass the symbol.
+  // Examples:
+  // convertToTradingSymbol('NIFTY SEP FUT'); //Output will be - NIFTYSEP20FUT
+  // convertToTradingSymbol('NIFTY SEP 21 FUT'); //Output will be - NIFTYSEP21FUT
+
+  // Below both will have the same output.
+  // convertToTradingSymbol('NIFTY 1 OCT 11300 CE'); //Output will be - NIFTY20O0111300CE
+  // convertToTradingSymbol('NIFTY 1 OCT 20 11300 CE'); //Output will be - NIFTY20O0111300CE
+
+  // Below both will have the same output.
+  // convertToTradingSymbol('NIFTY OCT 20 11300 CE'); //Output will be - NIFTY20OCT11300CE
+  // convertToTradingSymbol('NIFTY OCT 11300 CE'); //Output will be - NIFTY20OCT11300CE
+
+  // convertToTradingSymbol('RELIANCE'); //Output will be - RELIANCE
+
+  // convertToTradingSymbol('USDINR 8 OCT 74.25 CE'); //Output will be - USDINR20O0874.25CE
+
+  // convertToTradingSymbol('CRUDEOIL OCT FUT'); //Output will be - CRUDEOIL20OCTFUT
+  // convertToTradingSymbol('CRUDEOIL OCT 21 FUT'); //Output will be - CRUDEOIL21OCTFUT
+
+  // ************************************************************************************************
+
+  convertToTradingSymbol = (stringSymbol) => {
+    const months = [
+      "M",
+      "JAN",
+      "FEB",
+      "MAR",
+      "APR",
+      "MAY",
+      "JUN",
+      "JUL",
+      "AUG",
+      "SEP",
+      "OCT",
+      "NOV",
+      "DEC",
+    ];
+
+    let scrip = stringSymbol.toUpperCase().split(" ");
+    //find fut/ce/pe
+    let scripType = scrip[scrip.length - 1];
+    let isFO = scripType == "CE" || scripType == "PE" || scripType == "FUT";
+    if (!isFO) return stringSymbol;
+    var ex;
+    //insert year in ce/pe
+    scrip = this.insertYear(scrip, scripType);
+
+    //convert month as per weekly/monthly option
+    if (scripType == "CE" || scripType == "PE") {
+      //is weekly
+      if (this.isNumeric(scrip[1])) {
+        //YEAR MONTH_first_letter DATE STRIKE CE/PE
+        //2021 J 07 14000 CE
+        // ex = `${scrip[3]}${scrip[2].slice(0, 1)}` + `0${scrip[1]}`.slice(-2) + `${Number(scrip[4])}${scrip[5]}`;
+        //06 JAN 22 19000 PE
+        // ex = `0${scrip[1]}`.slice(-2) + `${scrip[2]}` + `${scrip[3]}` + `${Number(scrip[4])}${scrip[5]}`;
+
+        // 22 1  06 16900 PE
+
+        ex =
+          `${String(scrip[3]).slice(-2)}${months.indexOf(scrip[2])}` +
+          `0${scrip[1]}`.slice(-2) +
+          `${Number(scrip[4])}${scrip[5]}`;
+      } else {
+        ex = `${scrip[2]}${scrip[1]}${Number(scrip[3])}${scrip[4]}`;
+      }
+    } else {
+      ex = `${scrip[2]}${scrip[1]}FUT`;
     }
 
-    insertYear = (scrip, scripType) => {
-        var _year = 22;
-
-        let year_index = scripType == 'FUT' ? -2 : -3;
-        let oldYear = scrip.slice(year_index, year_index + 1)[0]
-        let is_year = this.isNumeric(oldYear);
-
-        if (is_year) return scrip;
-
-        return this.insertInToArray(scrip, year_index + 1, _year);
-    }
-
-
-    // ************************************************************************************************
-    // convertToTradingSymbol
-    //
-    // Validate the input of the order send on the bot.
-    // It will have one arguments.
-    // 1. Pass the symbol.
-    // Examples:
-    // convertToTradingSymbol('NIFTY SEP FUT'); //Output will be - NIFTYSEP20FUT
-    // convertToTradingSymbol('NIFTY SEP 21 FUT'); //Output will be - NIFTYSEP21FUT
-
-    // Below both will have the same output.
-    // convertToTradingSymbol('NIFTY 1 OCT 11300 CE'); //Output will be - NIFTY20O0111300CE
-    // convertToTradingSymbol('NIFTY 1 OCT 20 11300 CE'); //Output will be - NIFTY20O0111300CE
-
-    // Below both will have the same output.
-    // convertToTradingSymbol('NIFTY OCT 20 11300 CE'); //Output will be - NIFTY20OCT11300CE
-    // convertToTradingSymbol('NIFTY OCT 11300 CE'); //Output will be - NIFTY20OCT11300CE
-
-    // convertToTradingSymbol('RELIANCE'); //Output will be - RELIANCE
-
-    // convertToTradingSymbol('USDINR 8 OCT 74.25 CE'); //Output will be - USDINR20O0874.25CE
-
-    // convertToTradingSymbol('CRUDEOIL OCT FUT'); //Output will be - CRUDEOIL20OCTFUT
-    // convertToTradingSymbol('CRUDEOIL OCT 21 FUT'); //Output will be - CRUDEOIL21OCTFUT
-
-    // ************************************************************************************************
-
-    convertToTradingSymbol = (stringSymbol) => {
-        const months = ['M', "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-
-        let scrip = stringSymbol.toUpperCase().split(' ');
-        //find fut/ce/pe
-        let scripType = scrip[scrip.length - 1];
-        let isFO = scripType == 'CE' || scripType == 'PE' || scripType == 'FUT';
-        if (!isFO) return stringSymbol;
-        var ex;
-        //insert year in ce/pe
-        scrip = this.insertYear(scrip, scripType);
-
-        //convert month as per weekly/monthly option
-        if (scripType == 'CE' || scripType == 'PE') {
-
-            //is weekly
-            if (this.isNumeric(scrip[1])) {
-                //YEAR MONTH_first_letter DATE STRIKE CE/PE
-                //2021 J 07 14000 CE
-                ex = `${scrip[3]}${scrip[2].slice(0, 1)}` + `0${scrip[1]}`.slice(-2) + `${Number(scrip[4])}${scrip[5]}`;
-                //06 JAN 22 19000 PE
-                // ex = `0${scrip[1]}`.slice(-2) + `${scrip[2]}` + `${scrip[3]}` + `${Number(scrip[4])}${scrip[5]}`;
-
-                // 22 1  06 16900 PE
-
-                // ex = `${String(scrip[3]).slice(-2)}${months.indexOf(scrip[2])}` + `0${scrip[1]}`.slice(-2) + `${Number(scrip[4])}${scrip[5]}`;
-            } else {
-                ex = `${scrip[2]}${scrip[1]}${Number(scrip[3])}${scrip[4]}`;
-            }
-        } else {
-            ex = `${scrip[2]}${scrip[1]}FUT`;
-        }
-
-        return `${scrip[0]}${ex}`;
-    }
-
+    return `${scrip[0]}${ex}`;
+  };
 }
 
-
-module.exports = ConvertToSymbol
+module.exports = ConvertToSymbol;
--- example/test.js
+++ example/test.js
@@ -1,68 +1,67 @@
-const _udf = require('../index');
+const _udf = require("../index");
 
 let udf = new _udf();
 
 const delay = async () => {
-    console.log('Hello1');
-    await udf.delay(2000);
-    console.log('Hello after 2 sec');
-}
+  console.log("Hello1");
+  await udf.delay(2000);
+  console.log("Hello after 2 sec");
+};
 
 const stringToJSON = () => {
-    let output = udf.stringToJSON('fname=deepak\nlname=kumar');
-    console.log(output);
-}
+  let output = udf.stringToJSON("fname=deepak\nlname=kumar");
+  console.log(output);
+};
 
 const isAlphaNumeric = () => {
-    let output = udf.isAlphaNumeric('abcdxtyz', false);
-    console.log(output);
-}
+  let output = udf.isAlphaNumeric("abcdxtyz", false);
+  console.log(output);
+};
 
 const insertInToArray = () => {
-    let output = udf.insertInToArray(['A', 'C', 'D'], 1, 'B');
-    console.log(output);
-}
+  let output = udf.insertInToArray(["A", "C", "D"], 1, "B");
+  console.log(output);
+};
 
 const convertToTradingSymbol = () => {
-    let output = udf.convertToTradingSymbol('NIFTY 6 oct 22 16900 CE');
-    // let output = udf.convertToTradingSymbol('NIFTY OCT 18000 PE');
-    console.log(output);
-}
+  //   let output = udf.convertToTradingSymbol("NIFTY 5 jan 23 18100 CE");
+  let output = udf.convertToTradingSymbol("NIFTY JAN 23 18100 CE");
+  console.log(output);
+};
 
 const toBuffer = () => {
-    let output = udf.toBuffer('abc');
-    console.log(output);
-}
+  let output = udf.toBuffer("abc");
+  console.log(output);
+};
 
 const toSTR = () => {
-    let output = udf.toSTR('YWJj');
-    console.log(output);
-}
+  let output = udf.toSTR("YWJj");
+  console.log(output);
+};
 
 const properCase = () => {
-    let output = udf.properCase('YWJj');
-    console.log(output);
-}
+  let output = udf.properCase("YWJj");
+  console.log(output);
+};
 
 const convertToTable = async () => {
-    try {
-        console.log(udf.prototype);
-        let output = await udf.convertToTable(msg_arr, null, false, false, " ");
-        console.log(output);
-    } catch (e) {
-        console.log(e);
-    }
-}
-
+  try {
+    console.log(udf.prototype);
+    let output = await udf.convertToTable(msg_arr, null, false, false, " ");
+    console.log(output);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 let msg_arr = [
-    ['Telegram ID', 'user_telegram_id'],
-    ['Name', 'toSTR(userfullname)'],
-    ['Mobile', 'mobile'],
-    ['Email', 'emailid'],
-    ['Payment', 'pg'],
-    ['Amount', 'rupee + amt'],
-    ['Validity', 'days' + ' days']
+  ["Telegram ID", "user_telegram_id"],
+  ["Name", "toSTR(userfullname)"],
+  ["Mobile", "mobile"],
+  ["Email", "emailid"],
+  ["Payment", "pg"],
+  ["Amount", "rupee + amt"],
+  ["Validity", "days" + " days"],
 ];
 
 let msg = `Done! Congratulations on your new bot. You will find it at t.me/webloginbot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
@@ -72,8 +71,8 @@ sd:AAE0lb2yevrfQ1jbMNlxdfddddddYdnZjFGKgd6oN-csd...
 Keep your token secure and store it safely, it can be used by anyone to control your bot.
 
 For a description of the Bot API, see this page: https://core.telegram.org/bots/api`;
-parseBOTToken = () => udf.parseBOTToken(msg)
+parseBOTToken = () => udf.parseBOTToken(msg);
 
 // parseBOTToken();
 
-convertToTradingSymbol()
+convertToTradingSymbol();
